a2c10078efc0a141ea4f29f43b52910b1dfce8ab
Maurizio D'Addona <mauritiusdadd@gmail.com>
2019-12-14 18:38:58 +0100

    Add socket and server

--- monnalisa.py
+++ monnalisa.py
@@ -1,5 +1,77 @@
 #!/usr/bin/env python
-from monnalisa import xyzgui
+
+import sys
+import time
+import argparse
+import socket
+import logging
+from monnalisa import xyzgui, xyz
+
 
 if __name__ == '__main__':
-    xyzgui.main()
+    parser = argparse.ArgumentParser(description='Controls Da Vinci printers')
+    parser.add_argument("--server", type=str, nargs='?', metavar='IPADDR',
+                        default=False,
+                        help="Start the program as a server on the address "
+                        "%(metavar) to control the printer over the network. "
+                        "If no address is specified the server will be "
+                        "reachable any address the machine happens to have.")
+    parser.add_argument("--server-port", metavar='PORT', type=int,
+                        default=2222, help="Set the port used to create the "
+                        "server. If no port is psecified then the default "
+                        "port 2222 is used to accept inbound connections.")
+    parser.add_argument("--printer-port", '-p', metavar='PORT', type=str,
+                        help="The port used to communicate with the priter.")
+    parser.add_argument("--baud", '-b', metavar='BAUDRATE', type=int,
+                        default=9600, help="Specify the baud rate of the "
+                        "serial connection tiwth the printer. If this option "
+                        "is not specified then the default vaule ob 9600 is "
+                        "used")
+
+    args = parser.parse_args()
+    if args.server is False:
+        xyzgui.main()
+    else:
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        clog = logging.StreamHandler()
+        logger.addHandler(clog)
+        clog.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s'
+        )
+        clog.setFormatter(formatter)
+
+        if args.server:
+            addr = args.server
+        else:
+            addr = ''
+
+        logger.info("Creating a server on %s:%d", addr, args.server_port)
+        srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        srv.bind((addr, args.server_port))
+        srv.listen(5)
+        client = None
+        printer = xyz.XYZPrinter()
+        try:
+            if not printer.connect(args.printer_port, args.baud, timeout=1):
+                sys.exit(1)
+            while True:
+                logger.info("Waiting for clients")
+                client, client_addr = srv.accept()
+                logger.info("New client accepted from %s", client_addr)
+                while client:
+                    print(client.recv(1024))
+                    time.sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            if client:
+                try:
+                    logging.info("Closing client %s", client.getpeername()[0])
+                    client.send(b'close')
+                except OSError:
+                    pass
+                else:
+                    client.close()
+            srv.close()
+            printer.stop()
--- monnalisa/xyz.py
+++ monnalisa/xyz.py
@@ -1,7 +1,9 @@
 import logging
 import threading
 import serial
+import socket
 import time
+import os
 
 
 F_COLORS = {
@@ -96,6 +98,98 @@ class GuiLogger(logging.Handler):
             print(self.format(record))
 
 
+class SocketPort(threading.Thread):
+
+    PACKET_START = b'<msg>'
+    PACKET_END = b'</msg>'
+
+    def __init__(self, url, timeout=1):
+        super().__init__()
+        info = url.split(':')
+        self.addr = info[0]
+        self.port = 2222
+        self.is_open = False
+        self._do_stop = False
+        self.timeout = timeout
+        self._lock = threading.Lock()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.buffer = ''
+        try:
+            self.port = int(info[1])
+        except (IndexError, ValueError):
+            pass
+
+        logging.info("server on %s %d", self.addr, self.port)
+        self.socket.connect((self.addr, self.port))
+        self.socket.settimeout(timeout)
+        self.is_open = True
+        self.start()
+
+    def __del__(self):
+        self.close()
+
+    def readline(self):
+        stme = time.time()
+        while '\n' not in self.buffer:
+            if time.time() - stme >= self.timeout:
+                return ''
+        line_start = self.buffer.find('\n')
+        line = self.buffer[:line_start]
+        self.buffer = self.buffer[line_start+1]
+        return line
+
+    def run(self):
+        buff = b''
+        logging.info("Running socket thread loop")
+        while self.socket and not self._do_stop:
+            try:
+                buff += self.socket.recv(1024)
+            except socket.timeout:
+                time.sleep(0.250)
+                continue
+            except OSError:
+                break
+
+            msg_start = buff.find(self.PACKET_START)
+            if msg_start < 0:
+                continue
+
+            msg_end = buff.find(self.PACKET_END, msg_start+1)
+            if msg_end < 0:
+                continue
+
+            self._parsemsg(buff[msg_start:msg_end])
+            buff = buff[msg_end+len(self.PACKET_END):]
+        self.write(b"close")
+        self._lock.acquire()
+        self.is_open = False
+        self.socket.close()
+        self.socket = None
+        self._lock.release()
+
+    def _parsemsg(self, message):
+        print(message)
+
+    def write(self, data):
+        if not self.socket or not self.is_open:
+            return False
+        msg = self.PACKET_START
+        msg += data
+        msg += self.PACKET_END
+        self._lock.acquire()
+        if self.socket and self.is_open:
+            try:
+                self.socket.sendall(msg)
+            except BrokenPipeError:
+                self._do_stop = True
+        self._lock.release()
+
+    def close(self):
+        logging.info("Waiting for socket thread to stop...")
+        self._do_stop = True
+        # self.join()
+
+
 class XYZPrinter(threading.Thread):
     """
     Abstraction layer that communicates with printer hardware
@@ -108,26 +202,35 @@ class XYZPrinter(threading.Thread):
     def __init__(self):
         super().__init__()
         self.port = None
-        self._stop = False
+        self._do_stop = False
         self._stopped = False
         self.start()
 
     def stop(self):
-        self._stop = True
+        self._do_stop = True
+        self.disconnect()
+        # self.join()
 
     def connect(self, port, baud=9600, **args) -> bool:
         try:
-            logging.info(f"Conneting to {port}@{baud}...")
-            self.port = serial.Serial(port, baud, **args)
-        except serial.SerialException as exc:
-            logging.warning(f"Connetion failed: {exc}")
+            logging.info(f"Conneting to %s@%d...", port, baud)
+            if os.path.exists(port):
+                self.port = serial.Serial(port, baud, **args)
+            else:
+                self.port = SocketPort(port, **args)
+        except (
+            serial.SerialException, socket.gaierror,
+            ConnectionRefusedError
+        ) as exc:
+            logging.error("Connetion failed on %s: %s", port, exc)
             self.port = None
             return False
         logging.info("Connected")
         return True
 
     def disconnect(self):
-        self.port.close()
+        if self.port:
+            self.port.close()
 
     def query(self, stat='a'):
         self.sendaction('a', func='query')
@@ -140,16 +243,16 @@ class XYZPrinter(threading.Thread):
         if arg:
             msg += f':{arg}'
         if self.port and self.port.is_open:
-            logging.debug(f"sending message: {msg}")
+            logging.debug("sending message: %s", msg)
             self.port.write(msg.encode())
 
     def message_callback(self, msg):
         # not implemented, please override
-        logging.debug(f"printer send: {msg}")
+        logging.debug("printer send: %s", msg)
 
     def run(self):
         self.stoped = False
-        while not self._stop:
+        while not self._do_stop:
             if self.port and self.port.is_open:
                 res = self.port.readline()
                 if res:
